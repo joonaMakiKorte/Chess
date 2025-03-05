5d7f39b2201bbb0798caadf2460ad7f450d1555f
Eetu Risikko <eetu.risikko@tuni.fi>
2025-03-05 15:49:12 +0200

    board done

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -13,11 +13,13 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+
 namespace Chess
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+    /// 
     public partial class MainWindow : Window
     {
         public MainWindow()
