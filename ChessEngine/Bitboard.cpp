fb79fcbc5d30db3eabaef6c08106e45d0b213185
Eetu Risikko <eetu.risikko@tuni.fi>
2025-03-11 11:13:00 +0200

    rook, bishop and queen logic fixed

--- ChessEngine/Bitboard.cpp
+++ ChessEngine/Bitboard.cpp
@@ -227,11 +227,41 @@ uint64_t Bitboard::getBishopMoves(int square) {
 		BISHOP_MOVES[square].bottom_left |
 		BISHOP_MOVES[square].bottom_right);
 
+	// White bishop
 	if (white_bishops & bishop_bitboard) {
-		return moves &= ~whitePieces(); // White bishop can't move onto white pieces
+		moves &= ~whitePieces(); // White bishop can't move onto white pieces
+
+		// Prevent moving through or behind other white pieces
+		if (BISHOP_MOVES[square].top_left & white_bishops) {
+			moves &= ~BISHOP_MOVES[square].top_left; // Don't move past other white pieces on the top-left diagonal
+		}
+		if (BISHOP_MOVES[square].top_right & white_bishops) {
+			moves &= ~BISHOP_MOVES[square].top_right; // Don't move past other white pieces on the top-right diagonal
+		}
+		if (BISHOP_MOVES[square].bottom_left & white_bishops) {
+			moves &= ~BISHOP_MOVES[square].bottom_left; // Don't move past other white pieces on the bottom-left diagonal
+		}
+		if (BISHOP_MOVES[square].bottom_right & white_bishops) {
+			moves &= ~BISHOP_MOVES[square].bottom_right; // Don't move past other white pieces on the bottom-right diagonal
+		}
 	}
+	// Black bishop
 	else if (black_bishops & bishop_bitboard) {
-		return moves &= ~blackPieces(); // Black bishop can't move onto black pieces
+		moves &= ~blackPieces(); // Black bishop can't move onto black pieces
+
+		// Prevent moving through or behind other black pieces
+		if (BISHOP_MOVES[square].top_left & black_bishops) {
+			moves &= ~BISHOP_MOVES[square].top_left; // Don't move past other black pieces on the top-left diagonal
+		}
+		if (BISHOP_MOVES[square].top_right & black_bishops) {
+			moves &= ~BISHOP_MOVES[square].top_right; // Don't move past other black pieces on the top-right diagonal
+		}
+		if (BISHOP_MOVES[square].bottom_left & black_bishops) {
+			moves &= ~BISHOP_MOVES[square].bottom_left; // Don't move past other black pieces on the bottom-left diagonal
+		}
+		if (BISHOP_MOVES[square].bottom_right & black_bishops) {
+			moves &= ~BISHOP_MOVES[square].bottom_right; // Don't move past other black pieces on the bottom-right diagonal
+		}
 	}
 
 	return 0ULL; // Should never reach here
@@ -249,12 +279,46 @@ uint64_t Bitboard::getRookMoves(int square) {
 		ROOK_MOVES[square].left |
 		ROOK_MOVES[square].right);
 
+	// White rook
 	if (white_rooks & rook_bitboard) {
-		return moves &= ~whitePieces(); // White rook can't move onto white pieces
+		moves &= ~whitePieces(); // White rook can't move onto white pieces
+
+		// Prevent moving through or behind other white pieces
+		if (ROOK_MOVES[square].top & white_rooks) {
+			moves &= ~ROOK_MOVES[square].top; // Don't move past other white pieces in the top direction
+		}
+		if (ROOK_MOVES[square].bottom & white_rooks) {
+			moves &= ~ROOK_MOVES[square].bottom; // Don't move past other white pieces in the bottom direction
+		}
+		if (ROOK_MOVES[square].left & white_rooks) {
+			moves &= ~ROOK_MOVES[square].left; // Don't move past other white pieces on the left
+		}
+		if (ROOK_MOVES[square].right & white_rooks) {
+			moves &= ~ROOK_MOVES[square].right; // Don't move past other white pieces on the right
+		}
 	}
+	// Black rook
 	else if (black_rooks & rook_bitboard) {
-		return moves &= ~blackPieces(); // Black rook can't move onto black pieces
+		moves &= ~blackPieces(); // Black rook can't move onto black pieces
+
+		// Prevent moving through or behind other black pieces
+		if (ROOK_MOVES[square].top & black_rooks) {
+			moves &= ~ROOK_MOVES[square].top; // Don't move past other black pieces in the top direction
+		}
+		if (ROOK_MOVES[square].bottom & black_rooks) {
+			moves &= ~ROOK_MOVES[square].bottom; // Don't move past other black pieces in the bottom direction
+		}
+		if (ROOK_MOVES[square].left & black_rooks) {
+			moves &= ~ROOK_MOVES[square].left; // Don't move past other black pieces on the left
+		}
+		if (ROOK_MOVES[square].right & black_rooks) {
+			moves &= ~ROOK_MOVES[square].right; // Don't move past other black pieces on the right
+		}
 	}
+
+
+
+
 
 	return 0ULL; // Should never reach here
 }
@@ -275,11 +339,65 @@ uint64_t Bitboard::getQueenMoves(int square) {
 		QUEEN_MOVES[square].bottom_left |
 		QUEEN_MOVES[square].bottom_right);
 
-	if (white_queen & queen_bitboard) { // White queen
-		return moves &= ~whitePieces();
+	// White queen
+	if (white_queen & queen_bitboard) {
+		moves &= ~whitePieces(); // White queen can't move onto white pieces
+
+		// Prevent moving through or behind other white pieces
+		if (QUEEN_MOVES[square].top & white_queen) {
+			moves &= ~QUEEN_MOVES[square].top; // Don't move past other white pieces in the top direction
+		}
+		if (QUEEN_MOVES[square].bottom & white_queen) {
+			moves &= ~QUEEN_MOVES[square].bottom; // Don't move past other white pieces in the bottom direction
+		}
+		if (QUEEN_MOVES[square].left & white_queen) {
+			moves &= ~QUEEN_MOVES[square].left; // Don't move past other white pieces on the left
+		}
+		if (QUEEN_MOVES[square].right & white_queen) {
+			moves &= ~QUEEN_MOVES[square].right; // Don't move past other white pieces on the right
+		}
+		if (QUEEN_MOVES[square].top_left & white_queen) {
+			moves &= ~QUEEN_MOVES[square].top_left; // Don't move past other white pieces on the top-left diagonal
+		}
+		if (QUEEN_MOVES[square].top_right & white_queen) {
+			moves &= ~QUEEN_MOVES[square].top_right; // Don't move past other white pieces on the top-right diagonal
+		}
+		if (QUEEN_MOVES[square].bottom_left & white_queen) {
+			moves &= ~QUEEN_MOVES[square].bottom_left; // Don't move past other white pieces on the bottom-left diagonal
+		}
+		if (QUEEN_MOVES[square].bottom_right & white_queen) {
+			moves &= ~QUEEN_MOVES[square].bottom_right; // Don't move past other white pieces on the bottom-right diagonal
+		}
 	}
-	else if (black_queen & queen_bitboard) { // Black queen
-		return moves &= ~blackPieces();
+	// Black queen
+	else if (black_queen & queen_bitboard) {
+		moves &= ~blackPieces(); // Black queen can't move onto black pieces
+
+		// Prevent moving through or behind other black pieces
+		if (QUEEN_MOVES[square].top & black_queen) {
+			moves &= ~QUEEN_MOVES[square].top; // Don't move past other black pieces in the top direction
+		}
+		if (QUEEN_MOVES[square].bottom & black_queen) {
+			moves &= ~QUEEN_MOVES[square].bottom; // Don't move past other black pieces in the bottom direction
+		}
+		if (QUEEN_MOVES[square].left & black_queen) {
+			moves &= ~QUEEN_MOVES[square].left; // Don't move past other black pieces on the left
+		}
+		if (QUEEN_MOVES[square].right & black_queen) {
+			moves &= ~QUEEN_MOVES[square].right; // Don't move past other black pieces on the right
+		}
+		if (QUEEN_MOVES[square].top_left & black_queen) {
+			moves &= ~QUEEN_MOVES[square].top_left; // Don't move past other black pieces on the top-left diagonal
+		}
+		if (QUEEN_MOVES[square].top_right & black_queen) {
+			moves &= ~QUEEN_MOVES[square].top_right; // Don't move past other black pieces on the top-right diagonal
+		}
+		if (QUEEN_MOVES[square].bottom_left & black_queen) {
+			moves &= ~QUEEN_MOVES[square].bottom_left; // Don't move past other black pieces on the bottom-left diagonal
+		}
+		if (QUEEN_MOVES[square].bottom_right & black_queen) {
+			moves &= ~QUEEN_MOVES[square].bottom_right; // Don't move past other black pieces on the bottom-right diagonal
+		}
 	}
 
 	return 0ULL; // Should never reach here
